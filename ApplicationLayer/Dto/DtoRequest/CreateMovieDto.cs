6a8e1ef2cf91e2f19fb6f3d284e80006e5e3b029
Rumcajs121 <z.miara45@gmail.com>
2024-06-02 21:42:19 +0200

    fix(NewRazorComponentGetAll) I createt test layout for my getall method. I notes for my new ideas for Create new movie form i used Blob Storage and YT api this task

---
+++ ApplicationLayer/Dto/DtoRequest/CreateMovieDto.cs
@@ -0,0 +1,18 @@
+﻿using DomainLayer;
+
+namespace ApplicationLayer;
+
+public class CreateMovieDto
+{
+    public required string Title { get; set; }
+    public required string Director { get; set; }
+    public required string ReleaseDate { get; set; }
+
+    //Stworzenie dto dla photo do tego formularzu + konfiguracja całego blob storage
+    //Create movie zawiera dane + dynamicze dodawanie WASM zdjec (formularz w formularzu)
+    //Wybieranie coś na wzór enum Category plus Genre 
+    //Trailer dodawany bezpośrednio w widokufilmu  (dodanie sekcji) ??  Nullable dla trailer ?? 
+    //Trailer dodawany  w formularzu poprzez link youtube WASM napisanie metody pobierajacej z api YT i umeiszczajacy miniature.
+    //Kategorie pobiore z dbcotextu nastepnie uzytkownik bedzie mógł wybraćkategorie 
+    //po czym zosatnie zwrócone GenreId i przypisane do modelu
+}
