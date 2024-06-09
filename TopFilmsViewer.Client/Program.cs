ba2bf8ab9900fb77aee668fa8fc96a25fc276cc2
Rumcajs121 <z.miara45@gmail.com>
2024-06-09 09:00:15 +0200

    fix: I restore last commit becaus my program is down.

--- TopFilmsViewer.Client/Program.cs
+++ TopFilmsViewer.Client/Program.cs
@@ -1,5 +1,10 @@
+using ApplicationLayer;
+using InfrastructureLayer.Repository;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
+builder.Services.AddScoped(http=>new HttpClient{
+    BaseAddress=new Uri(builder.HostEnvironment.BaseAddress)
+});
+builder.Services.AddScoped<ITopFilmsViewer,TopFilmsViewerRepository>();
 await builder.Build().RunAsync();
